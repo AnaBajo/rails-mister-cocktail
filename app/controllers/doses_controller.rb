8ac82d062fbaebb3f28a22b320ce7a8ee0fe22e5
Ana Bajo <kozicana@yahoo.com>
2020-11-26 17:10:56 +0100

    derpy cocktail list

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,29 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    # find a way to get the cockt_id from the params
+    # find the cocktail itself with the id
+    # then connect the dose with the cocktail
+    # assign a cocktail to the dose
+
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(doses_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
